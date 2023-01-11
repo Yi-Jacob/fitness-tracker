ae17d6f63a2703da675c75b1f823bcf89c713e05
Yi-Jacob <jakeyi00@gmail.com>
2023-01-11 12:27:28 -0800

    in progress

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -9,7 +9,8 @@ export class TrainingService {
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
+    { id: 'cardio', name: 'Cardio', duration: 12000, calories: 200 }
   ];
   private runningExercise: Exercise;
   private exercises: Exercise[] = [];
